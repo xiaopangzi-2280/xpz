ca1ae0895ca082d7a94e8e620bb5a9c748af3588
xiaopangzi-2280 <155873044+xiaopangzi-2280@users.noreply.github.com>
2024-01-08 12:52:19 +0800

    Add files via upload

---
+++ 远程抓拍.py
@@ -0,0 +1,36 @@
+#导入请求模块
+import requests
+import webbrowser
+
+#经典开场白
+print("【欢迎使用 GitHub：xiaopangzi-2280】"+"\n")
+
+#💩山
+API = "https://uso.link/?"
+ckAPI = "http://uso.link/ck.php?id="
+
+off = input("您要使用还是查询？【使用/查询】：")
+if off == "查询":
+    cxid = input("请输入ID进行查询：")
+    idcx = (ckAPI+cxid)
+    print("查看图片请访问："+idcx)
+    print("---本轮脚本已结束---")
+    
+    
+id = input("请输入一个ID：")
+url = input("请输入点击后跳转的链接【默认链接请直接按回车】:")
+
+params = {
+    "id":id,
+    "url1":url
+}
+
+if url == "":
+    mr = (API+"id="+id+"&url="+"https://im.q.com"+"\n")
+    print(mr)
+    print("---本轮脚本已结束---")
+else:
+    diy = (API+"id="+id+"&url="+url+"\n")
+    print(diy)
+    print("---本轮脚本已结束---")
+    
